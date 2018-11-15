cae43310a127465645bc27112ef1649d0987fe36
Mark <Mark@SurfacePro>
2018-11-15 18:59:30 +0000

    Create and return Restaurant model

--- OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using OdeToFood.Models;
 
 namespace OdeToFood.Controllers
 {
@@ -6,7 +7,8 @@ namespace OdeToFood.Controllers
     {
         public IActionResult Index()
         {
-            return Content("Hello from the HomeController!");
+            var model = new Restaurant { Id = 1, Name = "Mark's Sushi Place" };
+            return new ObjectResult(model);
         }
     }
 }
