a852d64a28d3d8e8720a43d20fa65a1f6ed45aca
Mark <Mark@SurfacePro>
2018-11-15 22:16:39 +0000

    Create and display mock data

---
+++ OdeToFood/Services/InMemoryRestaurantData.cs
@@ -0,0 +1,28 @@
+﻿using OdeToFood.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Services
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> _restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            _restaurants = new List<Restaurant>
+            {
+                new Restaurant { Id = 1, Name = "Mark's Sushi Place" },
+                new Restaurant { Id = 2, Name = "Bob's Oyster Bar" },
+                new Restaurant { Id = 3, Name = "Ella's Chicken Coop" },
+            };
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return _restaurants.OrderBy(r => r.Name);
+        }
+    }
+}
