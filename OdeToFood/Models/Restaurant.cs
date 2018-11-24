81dd752d4c766a34e9c8f380f11739a330987383
Mark <Mark@SurfacePro>
2018-11-24 11:55:20 +0000

    Create and use database (migrations created with dotnet cli)

--- OdeToFood/Models/Restaurant.cs
+++ OdeToFood/Models/Restaurant.cs
@@ -1,9 +1,17 @@
-﻿namespace OdeToFood.Models
+﻿using System.ComponentModel.DataAnnotations;
+using OdeToFood.Services;
+
+namespace OdeToFood.Models
 {
     public class Restaurant
     {
         public int Id { get; set; }
+
+        [Required, MaxLength(80)]
+        [Display(Name="Restaurant Name")]
         public string Name { get; set; }
+
+        [RequiredEnum(ErrorMessage = "The Cuisine type is required.")]
         public CuisineType Cuisine { get; set; }
     }
 }
--- OdeToFood/Startup.cs
+++ OdeToFood/Startup.cs
@@ -6,20 +6,30 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using OdeToFood.Data;
 using OdeToFood.Services;
 
 namespace OdeToFood
 {
     public class Startup
     {
+        private IConfiguration _config;
+
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddSingleton<IGreeter, Greeter>();
-            services.AddSingleton<IRestaurantData, InMemoryRestaurantData>();
+            services.AddDbContext<OdeToFoodDbContext>(options => options.UseSqlServer(_config.GetConnectionString("OdeToFood")));
+            services.AddScoped<IRestaurantData, SqlRestaurantData>();
             services.AddMvc();
         }
 
